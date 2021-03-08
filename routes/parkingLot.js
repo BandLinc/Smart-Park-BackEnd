2d6a0ac2ce13da31e69a24fa0be01251a72d369d
LincolnLidanya <lincolnlidanya@gmail.com>
2021-03-08 21:22:25 +0300

    New changes

--- routes/parkingLot.js
+++ routes/parkingLot.js
@@ -1,47 +1,45 @@
-const express=require ('express')
-const router=express.Router()
+const express = require("express");
+const router = express.Router();
 
-const PackingLot=require('../models/parkinglot')
-
+const PackingLot = require("../models/parkinglot");
 
 //Get all Parking Lots
-router.get('/',async(req,res)=>{
-    try {
-        const packinglots=await PackingLot.find()
-        res.json(packinglots)
-    }catch {
-        res.send('Error :'+err)
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const packinglots = await PackingLot.find();
+    res.json(packinglots);
+  } catch {
+    res.send("Error :" + err);
+  }
+});
 
 //Add New Parking Lot
-router.post ('/',async(req,res)=>{
-    const packing_lot = new PackingLot ({
-        name:req.body.name,
-        initial:req.body.initial,
-        total_spots:req.body.total_spots,
-        created_at:req.body.created_at
-    })  
-    try {
-        const a1=await packing_lot.save()
-        res.json(a1)
-    }catch (err) {
-       res.send('Error : '+err)
-    }
-}) 
+router.post("/", async (req, res) => {
+  const packing_lot = new PackingLot({
+    name: req.body.name,
+    initial: req.body.initial,
+    total_spots: req.body.total_spots,
+    created_at: req.body.created_at,
+  });
+  try {
+    const a1 = await packing_lot.save();
+    res.json(a1);
+  } catch (err) {
+    res.send("Error Detected: " + err);
+  }
+});
 
 //Update the number of parking lots
-router.patch('/:id',async(req,res)=>{
-    try {
-        const parkinglot=await PackingLot.findById(req.params.id)
-        parkinglot.total_spots=req.body.total_spots
-        parkinglot.created_at=Date.now()
-        const a1=await parkinglot.save()
-        res.json(a1)
-    }catch {
-        res.send('Error :'+err)
-    }
-})
+router.patch("/:id", async (req, res) => {
+  try {
+    const parkinglot = await PackingLot.findById(req.params.id);
+    parkinglot.total_spots = req.body.total_spots;
+    parkinglot.created_at = Date.now();
+    const a1 = await parkinglot.save();
+    res.json(a1);
+  } catch {
+    res.send("Error :" + err);
+  }
+});
 
-module.exports=router
-
+module.exports = router;
